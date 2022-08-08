45596dfd22cea641b1df899d8e51e78d6c9b32e4
Bobby Joe Smith III <bobbyjoesmithiii@MacBook-Pro-4.lan>
2022-08-07 19:29:07 -0700

    Refactor Javascript Code

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-const acc = document.getElementsByClassName("accordion");
-
-for (let project of acc) {
-    project.addEventListener("click", function () {
-        const arrow = this.getElementsByTagName("span");
-        arrow[0].classList.toggle("rotate");
-        const panel = this.nextElementSibling;
-        panel.style.display === "block" ? panel.style.display = "none" : panel.style.display = "block";
-    });
-}
+const acc = document.getElementById("accordion");
+acc.addEventListener("click", function() {
+    console.log("click");
+    const arrow = document.getElementById("carron");
+    arrow.classList.toggle("rotate");
+    const panel = this.nextElementSibling;
+    panel.style.display === "block" ? panel.style.display = "none" : panel.style.display = "block";
+});

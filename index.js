07718c9eec858e150a475e1eb37d72765b9979e8
Hamzatrah <41709149+Hamzatrah@users.noreply.github.com>
2022-08-10 06:33:23 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,8 @@
-const initGui = () => {
-  const controls = document.getElementById('controls');
-  let buttonsText = '';
-  buttonsText += '<button id="button">Button</button>';
-  controls.innerHTML = buttonsText;
-}
-initGui();
-//////////////////////////////////
-// GUI Code end
-//////////////////////////////////
-
 const acc = document.getElementById("accordion");
-acc.addEventListener("click", function() {
-    console.log("click");
-    const arrow = document.getElementById("carron");
-    arrow.classList.toggle("rotate");
-    const panel = this.nextElementSibling;
-    panel.style.display === "block" ? panel.style.display = "none" : panel.style.display = "block";
-});
+ acc.addEventListener("click", function() {
+     console.log("click");
+     const arrow = document.getElementById("carron");
+     arrow.classList.toggle("rotate");
+     const panel = this.nextElementSibling;
+     panel.style.display === "block" ? panel.style.display = "none" : panel.style.display = "block";
+ });

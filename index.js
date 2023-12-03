8e1833d01954cef34054fc16e022b24a27e82935
Hamzatrah <41709149+Hamzatrah@users.noreply.github.com>
2023-12-03 22:02:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quantum Snake Game</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Quantum Snake Game</h1>
+    <canvas id="gameCanvas"></canvas>
+    <div id="gameInfo">
+        <button id="rollDieButton" onclick="rollQuantumDie()">Roll Quantum Die</button>
+        <div id="dieResult">Die Result: <span id="dieOutcome"></span></div>
+        <div id="specialMessage"></div>
+    </div>
+    <div id="mobileControls">
+        <button onclick="changeDirection('Up')">Up</button>
+        <button onclick="changeDirection('Down')">Down</button>
+        <button onclick="changeDirection('Left')">Left</button>
+        <button onclick="changeDirection('Right')">Right</button>
+    </div>
+    <script src="script.js"></script>
+</body>
+</html>
